2a59d0e8cf84e077a03aa81d51ee99804c0160f7
DarlaSolis <darladsolis2@gmail.com>
2025-01-10 13:31:04 -0600

    Creando el método atacar

--- poo_2c.py
+++ poo_2c.py
@@ -1,23 +1,53 @@
 class Personaje:
     #Atributos de la clase
-    nombre = 'Default'
-    fuerza = 0
-    inteligencia = 0
-    defensa = 0
-    vida = 0
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
+        self.nombre = nombre
+        self.fuerza = fuerza
+        self.inteligencia = fuerza
+        self.defensa = defensa
+        self.vida = vida
 
-# variable del constructor vacio 
-mi_personaje = Personaje()
+    def imprimir_atributos(self):
+        print(self.nombre)
+        print("-Fuerza:", self.fuerza)
+        print("-Inteligencia:", self.inteligencia)
+        print("-Defensa:", self.defensa)
+        print("-Vida:", self.vida)
+    
+    def subir_nivel(self, fuerza, inteligencia, defensa):
+        self.fuerza = self.fuerza + fuerza 
+        self.inteligencia = self.inteligencia + inteligencia
+        self.defensa += defensa
+    
+    def esta_vivo(self):
+        return self.vida > 0
 
-print("El nombre de mi personaje es: ", mi_Personaje.nombre)
-print("La fuerza de mi personaje es: ", mi_Personaje.fuerza)
-print("La inteligencia personaje es: ", mi_Personaje.inteligencia)
-print("La defensa personaje es: ", mi_Personaje.defensa)
-print("La vida mi personaje es: ", mi_Personaje.vida)
+    def morir(self):
+        self.vida = 0
+        print(self.nombre, "ha muerto")
+
+    def dañar(self, enemigo):
+        return self.fuerza - enemigo.defensa
+    
+    def atacar(self, enemigo):
+        daño = self.dañar(enemigo)
+        enemigo.vida = enemigo.vida - daño
+        print(self.nombre, 'ha realizado', daño, "puntos de daño a", enemigo.nombre)
+        print("Vida de", enemigo.nombre, "es", enemigo.vida)
+
+# variable del constructor 
+mi_personaje = Personaje("EsteBandido", 100, 50, 45, 100)
+mi_enemigo = Personaje("Angel", 70, 100, 40, 100 )
+mi_personaje.imprimir_atributos()
+mi_personaje.atacar(mi_enemigo)
+# mi_personaje.subir_nivel(15, 5, 10)
+# print("Valores actualizados")
+# mi_personaje.imprimir_atributos()
+
 
 #Modificando valores de los atributos
-mi_Personaje.nombre = "EstebanDido"
-mi_Personaje.defensa = 300
-mi_Personaje.fuerza = 30
-mi_Personaje.inteligencia = -2
-mi_Personaje.vida = 2
+
+
+
+
+
